0819408fb358dd7b0dbb20e3320e855c8391f51a
K Cory Jones <kcoryjones@gmail.com>
2015-11-26 13:27:30 -0700

    style api.js

--- generators/ang-aws/templates/api.js
+++ generators/ang-aws/templates/api.js
@@ -1,48 +1,62 @@
-angular.module('<%= _.camelCase(appname) %>').factory('api',function(angAws, $http) {
-    //create our service object
-	var api = {};
+(function() {
+    'use strict';
 
-	api.defaults = {
-		url: 'http://192.168.7.24:3002'
-	};
+    angular
+        .module('<%= _.camelCase(appname) %>')
+        .factory('api', api);
 
-	api.call = function(method, endpoint, data) {
-		var config = {
-			cache: false,
-			method: method,
-			url: api.defaults.url + endpoint
+    api.$inject = ['$http'];
+
+    function api($http) {
+    	var url  = 'http://192.168.7.24:3002';
+		var service = {
+            url: url,
+            put: put,
+            get: get,
+            post: post,
+            delete: delete,
+            login: login
+        };
+
+        return service;
+
+        //handles all methods
+        function call = function(method, endpoint, data) {
+			var config = {
+				cache: false,
+				method: method,
+				url: api.defaults.url + endpoint
+			}
+			if (data) {
+				config.data = data;
+			}
+			return $http(config);
+		};
+
+		//restful methods
+		function put = function(model, item) {
+			var endpoint = '/api/rest/' + model + '/' + item.id;
+			return api.call('PUT', endpoint, item);
+		};
+		function get = function(model, hash) {
+			var endpoint = '/api/rest/' + model;
+			if (hash)
+				endpoint += '/' + hash;
+			return api.call('GET', endpoint, null);
+		};
+		function post = function(model, hash, range) {
+			var endpoint = '/api/rest/' + model;
+			return api.call('POST', endpoint, item);
+		};
+		function delete = function(model, item) {
+			var endpoint = '/api/rest/' + model + '/' + item.id;
+			return api.call('DELETE', endpoint, item);
+		};
+
+		//login call
+		api.login = function(user) {
+			return api.call('POST', '/api/login', user);
 		}
-		if (data) {
-			config.data = data;
-		}
-		return $http(config);
-	};
 
-	//our crud
-	api.put = function(model, item) {
-		var endpoint = '/api/rest/' + model + '/' + item.id;
-		return api.call('PUT', endpoint, item);
-	};
-	api.get = function(model, hash) {
-		var endpoint = '/api/rest/' + model;
-		if (hash)
-			endpoint += '/' + hash;
-		return api.call('GET', endpoint, null);
-	};
-	api.post = function(model, hash, range) {
-		var endpoint = '/api/rest/' + model;
-		return api.call('POST', endpoint, item);
-	};
-	api.delete = function(model, item) {
-		var endpoint = '/api/rest/' + model + '/' + item.id;
-		return api.call('DELETE', endpoint, item);
-	};
-
-	//login
-	api.login = function(user) {
-		return api.call('POST', '/api/login', user);
-	}
-
-	//return service object
-	return api;
-});
+    }
+})();
